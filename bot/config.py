489443a35b45732b687740b2d3d4073137fdfd52
Kattupuchi <107940225+Kattupuchi@users.noreply.github.com>
2022-07-15 08:37:45 -0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-    SUDOS=os.environ['SUDOS']
-    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
-    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN', "5429740400:AAH8UdeZAwzTJhUq8b17MaGnhnjExJhSUqM"]
+    SUDOS=os.environ['SUDOS', "5599715194"]
+    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH', "8da8adb41f654ba374788eb88003c3c4"]
+    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID', "9531120"])
     
     if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
